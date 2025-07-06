9711229b403858d24e794be230a5f7ebe05e5097
hendricksonweib <hendricksonrocha22@gmail.com>
2025-07-06 11:10:18 -0300

    Feat: adiciona componente Footer ao projeto

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,5 +3,6 @@ export const DashboardPage = () => (
   <div className="flex items-center justify-center min-h-screen bg-white">
     <Header/>
     <h1 className="text-3xl font-bold text-blue-600">Bem-vindo ao Dashboard!</h1>
+    
   </div>
 );
